bec82f0373107ca55b7945ced627467fb3594c3e
Guillermo Martos <guille.l.martos@gmail.com>
2024-06-10 00:13:27 +0200

    + FEAT EXTRA fight route ready!

--- routes/fightRoutes.js
+++ routes/fightRoutes.js
@@ -1,7 +1,41 @@
 import { Router } from "express";
+import { tokenValidation } from "../middlewares/auth.tokenValidation.middleware.js";
+import { fightsService } from "../services/fightService.js";
+import {
+  createFightValid,
+  updateFightValid,
+} from "../middlewares/fight.validation.middleware.js";
 
 const router = Router();
 
 // OPTIONAL TODO: Implement route controller for fights
+router.post("/", tokenValidation, createFightValid, (req, res, next) => {
+  const { fighter1, fighter2 } = req.body;
+  try {
+    const fightCreated = fightsService.createFight(fighter1, fighter2);
+    res.body = fightCreated;
+    return next(req.body);
+  } catch (error) {
+    return next(error);
+  }
+});
+
+router.patch(
+  "/:id/log",
+  tokenValidation,
+  updateFightValid,
+  (req, res, next) => {
+    const id = req.params.id;
+    try {
+      const fightUpdated = fightsService.updateFight(id, {
+        ...req.body,
+      });
+      res.body = fightUpdated;
+      return next(req.body);
+    } catch (error) {
+      return next(error);
+    }
+  }
+);
 
 export { router };
